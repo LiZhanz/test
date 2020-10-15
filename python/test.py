31e07c017fcbff94a4d568ef02d7cd946a614516
li-zhan-zhan <lzzvia@126.com>
2020-10-15 13:34:27 +0800

    实验4 第一题

--- python/test.py
+++ python/test.py
@@ -1,4 +1,20 @@
+import re
 
+# 有一段英文文本，其中有单词连续重复了2次，编写程序检查重复的单词并只保留一个。
+def f1():
+    str = "This is is a dest."
+    pattern = re.compile(r'\b(\w+)(\s+\1){1,}\b')
+    matchResult = pattern.search(str)
+    str = pattern.sub(matchResult.group(1),str)
+    print(str)
+    print(matchResult.group(0))
 
-num = int(input("请输入3位以上的数字"))
-print(num//100)
+def f2():
+    x = 'This is a a desk.'
+    pattern = re.compile(r'(?P<f>\b\w+\b)\s(?P=f)')
+    matchResult = pattern.search(x)
+    x = x.replace(matchResult.group(0),matchResult.group(1))
+    print(x)
+if __name__ == "__main__":
+    f1()
+    f2()
